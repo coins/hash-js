2c0e8a6b8a54040188df1578f1dd4ea47e922a36
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-08 10:48:09 +0100

    Cleanup add JSDocs

--- src/hash.js
+++ src/hash.js
@@ -1,21 +1,53 @@
+/**
+ *
+ * Secure Hash Functions
+ * Simple wrappers for the WebCrypto API
+ *
+ */
+
+/**
+ *
+ * Computes the SHA256 hash of an input
+ * @param {ArrayBuffer} buffer The input
+ * @return {Promise<Uint8Array>} The hash of the input
+ *
+ */
 export function sha256(buffer) {
-    return crypto.subtle
-        .digest({ name: 'SHA-256' }, buffer)
+    return crypto.subtle.digest({ name: 'SHA-256' }, buffer)
         .then(new Uint8Array)
 }
 
+/**
+ *
+ * Computes the double SHA256 hash of an input
+ * @param {ArrayBuffer} buffer The input
+ * @return {Promise<Uint8Array>} The hash of the input
+ *
+ */
 export function sha256d(buffer) {
     return sha256(buffer).then(sha256);
 }
 
+/**
+ *
+ * Computes the SHA512 hash of an input
+ * @param {ArrayBuffer} buffer The input
+ * @return {Promise<Uint8Array>} The hash of the input
+ *
+ */
 export function sha512(buffer) {
-    return crypto.subtle
-        .digest({ name: 'SHA-512' }, buffer)
+    return crypto.subtle.digest({ name: 'SHA-512' }, buffer)
         .then(new Uint8Array)
 }
 
+/**
+ *
+ * Computes the SHA1 hash of an input
+ * @param {ArrayBuffer} buffer The input
+ * @return {Promise<Uint8Array>} The hash of the input
+ *
+ */
 export function sha1(buffer) {
-    return crypto.subtle
-        .digest({ name: 'SHA-1' }, buffer)
+    return crypto.subtle.digest({ name: 'SHA-1' }, buffer)
         .then(new Uint8Array)
 }
