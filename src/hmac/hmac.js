335e223d4f664872e60e1384b1f4ea1da686eb75
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-13 09:41:18 +0100

    Cleanup hmac

--- src/hmac/hmac.js
+++ src/hmac/hmac.js
@@ -12,11 +12,11 @@ import * as Buffer from '../../../buffer-js/src/buffer-utils/buffer-utils.js'
  *    @param {Number} outputSize - The output size of the underlying hash function (e.g. 20 bytes for SHA-1)
  *    @return {Bytes} - the HMAC
  */
-export async function hmac(key, message, hash, blockSize, outputSize) {
+export async function hmac(key, message, hashFn, blockSize, outputSize) {
 
     // Keys longer than blockSize are shortened by hashing them
     if (key.byteLength > blockSize) {
-        key = await hash.hash(key) // Key becomes outputSize bytes long
+        key = await hashFn.hash(key) // Key becomes outputSize bytes long
     }
 
     // Keys shorter than blockSize are padded to blockSize by padding with zeros on the right
@@ -24,11 +24,11 @@ export async function hmac(key, message, hash, blockSize, outputSize) {
         key = Buffer.padRight(key, blockSize) // Pad key with zeros to make it blockSize bytes long
     }
 
-    const innerPad = Buffer.xor(key, Buffer.repeat(0x36, blockSize))
-    const outerPad = Buffer.xor(key, Buffer.repeat(0x5c, blockSize))
+    const innerPaddedKey = Buffer.xor(key, Buffer.repeat(0x36, blockSize))
+    const outerPaddedKey = Buffer.xor(key, Buffer.repeat(0x5c, blockSize))
 
-    const innerHash = await hash.hash(Buffer.concat(innerPad, message))
-    const outerHash = await hash.hash(Buffer.concat(outerPad, innerHash))
+    const innerHash = await hashFn.hash(Buffer.concat(innerPaddedKey, message))
+    const outerHash = await hashFn.hash(Buffer.concat(outerPaddedKey, innerHash))
     return outerHash
 }
 
